3fd55bb04e1995edc97326c4ccec9934079fea2a
jasurbekShodmonov1 <jasurshodmonov21020@gmail.com>
2025-10-09 14:22:37 +0500

    Balance transation unittest

--- src/test/java/com/example/task/service/BalanceTransactionServiceTest.java
+++ src/test/java/com/example/task/service/BalanceTransactionServiceTest.java
@@ -4,8 +4,10 @@ package com.example.task.service;
 import com.example.task.dto.response.BalanceTransactionResponseDto;
 import com.example.task.dto.response.UserResponseDto;
 import com.example.task.entity.BalanceTransaction;
+import com.example.task.entity.User;
 import com.example.task.mapper.BalanceTransactionMapper;
 import com.example.task.repository.BalanceTransactionRepository;
+import com.example.task.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.module.ResolutionException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,6 +29,9 @@ public class BalanceTransactionServiceTest {
 
     @Mock
     private BalanceTransactionRepository balanceTransactionRepository;
+
+    @Mock
+    private UserRepository userRepository;
 
     @Mock
     private BalanceTransactionMapper balanceTransactionMapper;
@@ -114,4 +120,98 @@ public class BalanceTransactionServiceTest {
 
          verify(balanceTransactionMapper,never()).toDto(any());
     }
+
+    @Test
+    void  getByUser_validId_returnsTransaction(){
+
+        UUID userId = UUID.randomUUID();
+        User user = new User();
+
+        user.setId(userId);
+
+        BalanceTransaction transaction1 = mock(BalanceTransaction.class);
+        BalanceTransaction transaction2 = mock(BalanceTransaction.class);
+
+        UserResponseDto sender = new UserResponseDto(userId, "Alice", 100.0, "alice123");
+        UserResponseDto receiver = new UserResponseDto(UUID.randomUUID(), "Bob", 200.0, "bob321");
+
+        BalanceTransactionResponseDto dto1 =
+                new BalanceTransactionResponseDto(UUID.randomUUID(), new BigDecimal("10.0"), "coffee", sender, receiver);
+        BalanceTransactionResponseDto dto2 =
+                new BalanceTransactionResponseDto(UUID.randomUUID(), new BigDecimal("25.0"), "lunch", sender, receiver);
+
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(balanceTransactionRepository.findBySenderUser(user))
+                .thenReturn(List.of(transaction1, transaction2));
+        when(balanceTransactionMapper.toDto(transaction1)).thenReturn(dto1);
+        when(balanceTransactionMapper.toDto(transaction2)).thenReturn(dto2);
+
+        List<BalanceTransactionResponseDto> result = balanceTransactionService.getByUser(userId);
+
+        assertEquals(2, result.size());
+        assertEquals(new BigDecimal("10.0"), result.get(0).amount());
+        assertEquals("coffee", result.get(0).description());
+        assertEquals("lunch", result.get(1).description());
+
+        verify(userRepository).findById(userId);
+        verify(balanceTransactionRepository).findBySenderUser(user);
+        verify(balanceTransactionMapper, times(2)).toDto(any(BalanceTransaction.class));
+    }
+
+    @Test
+    void getByUser_ShouldThrowException_WhenUserNotFound(){
+        UUID userId = UUID.randomUUID();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        RuntimeException exception = assertThrows(
+                RuntimeException.class,
+                ()->balanceTransactionService.getByUser(userId)
+        );
+
+        assertEquals("user not found",exception.getMessage());
+
+        verify(balanceTransactionRepository, never()).findBySenderUser(any());
+        verify(balanceTransactionMapper, never()).toDto(any());
+    }
+
+    @Test
+    void getByDate_ShouldReturnTransactions_WhenDateBeforeToday(){
+        LocalDate date = LocalDate.now().minusDays(2);
+        LocalDate today = LocalDate.now();
+
+        BalanceTransaction transaction = new BalanceTransaction();
+        BalanceTransactionResponseDto dto =
+                new BalanceTransactionResponseDto(
+                        UUID.randomUUID(),
+                        BigDecimal.valueOf(100),
+                        "Test transaction",
+                        null,
+                        null
+                );
+
+        when(balanceTransactionRepository.findByTimestampBetween(date,today))
+                .thenReturn(List.of(transaction));
+        when(balanceTransactionMapper.toDto(transaction)).thenReturn(dto);
+
+        List<BalanceTransactionResponseDto> result = balanceTransactionService.getByDate(date);
+
+        assertEquals(1, result.size());
+        assertEquals("Test transaction", result.get(0).description());
+        verify(balanceTransactionRepository).findByTimestampBetween(date, today);
+        verify(balanceTransactionMapper).toDto(transaction);
+    }
+
+    @Test
+    void getByDate_ShouldReturnEmptyList_WhenDateIsTodayOrAfter(){
+        LocalDate today = LocalDate.now();
+
+        List<BalanceTransactionResponseDto>  result = balanceTransactionService.getByDate(today);
+
+        assertTrue(result.isEmpty());
+        verifyNoInteractions(balanceTransactionRepository);
+        verifyNoInteractions(balanceTransactionMapper);
+    }
+
 }
